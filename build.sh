00949f754d3ab200f2c2ab70360b3305ec83469d
Dylan Sale <dylan.sale@enabled.com.au>
2020-01-23 11:39:41 +1030

    added build.sh to push new versions to dockerhub

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+VERSION=1.1
+TAG=enabledsolutions/docx-template-to-pdf:${VERSION}
+
+docker build -t ${TAG} .
+docker push ${TAG}
+docker tag ${TAG} enabledsolutions/docx-template-to-pdf:latest
+docker push enabledsolutions/docx-template-to-pdf:latest
